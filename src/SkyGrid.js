d79a89f922335760fcd87343dd564b6a70e84949
tommitytom <tommitytom@gmail.com>
2016-10-17 00:03:30 +1100

    Small project fix

--- src/SkyGrid.js
+++ src/SkyGrid.js
@@ -1,12 +1,4 @@
-/*import Project from './Project';
-
-export default class SkyGrid {
-	static project(projectId, settings) {
-		return new Project(projectId, settings);
-	}
-}*/
-
-var Project = require('./project');
+var Project = require('./Project');
 
 exports.project = function(projectId, settings) {
 	return new Project.default(projectId, settings);
